9bc31cf770d8a4e62ad37b16ff4e61c65c8df503
vlad-gor95 <90379885+vlad-gor95@users.noreply.github.com>
2021-09-09 18:37:57 +0300

    Disable GyverOS and GyverPower

--- test_bench/test_bench.ino
+++ test_bench/test_bench.ino
@@ -1,9 +1,9 @@
 #define DHT11_PIN 2
 #define BUTTON_0_PIN 4
 
-#include <GyverOS.h>               // подключаем простой диспетчер задач
-GyverOS<5> OS;                     // количество подключенных задач - 5
-#include <GyverPower.h>                   // подключение библиотеки энергосбережения
+//#include <GyverOS.h>               // подключаем простой диспетчер задач
+//GyverOS<5> OS;                     // количество подключенных задач - 5
+//#include <GyverPower.h>                   // подключение библиотеки энергосбережения
 #include <Wire.h>                  // подключаем библиотеку для работы с I2C
 #include <DHT.h>                   // подключаем библиотеку для датчика DHT11
 DHT dht(DHT11_PIN, DHT11);         // сообщаем на каком порту и какой датчик будет работать
@@ -13,6 +13,8 @@ byte DHT11_Hum = 0;
 LiquidCrystal_PCF8574 lcd(0x27);   // Вариант для библиотеки PCF8574
 #include <EncButton.h>             // Подключение библиотеки для обработки кнопки
 EncButton<EB_TICK, BUTTON_0_PIN> btn(INPUT_PULLUP);
+#include "GyverTimer.h"            // Подключение библиотеки с таймерами
+GTimer secTimer(MS, 500);         // Таймер работающий раз в 500 мс
 
 
 
@@ -21,67 +23,76 @@ void setup() {
   dht.begin();               // запускаем датчик DHT11
   lcd.begin(16, 2);          // Экран 16 столбцов на 2 строки
   lcd.setBacklight(255);     // Установка максимальной яркости экрана
-
-  // порядковый номер задачи, имя функции, время в мс.
-  OS.attach(0, sensor_DHT11, 100);
-  OS.attach(1, Display0, 1000);
-  OS.attach(2, Display1, 1000);
-  OS.attach(3, Display2, 1000);
-  OS.attach(4, Display3, 1000);
+  /*
+    // порядковый номер задачи, имя функции, время в мс.
+    OS.attach(0, sensor_DHT11, 1000);
+    OS.attach(1, Display0, 1000);
+    OS.attach(2, Display1, 1000);
+    OS.attach(3, Display2, 1000);
+    OS.attach(4, Display3, 1000);
+  */
 }
 
 void sensor_DHT11() {
-  DHT11_temp = dht.readTemperature(); // Считываем температуру (t)
-  DHT11_Hum = dht.readHumidity();     // Cчитываем  влажность (h)
+  if (secTimer.isReady()) {
+    DHT11_temp = dht.readTemperature(); // Считываем температуру (t)
+    DHT11_Hum = dht.readHumidity();     // Cчитываем  влажность (h)
+  }
 }
 
 void Display0() {
-  /*
-    float t = dht.readTemperature(); // Считываем температуру (t)
-    byte h = dht.readHumidity();     // Cчитываем  влажность (h)
-  */
-   // выводим температуру (t) и влажность (h) на монитор порта (дебаг) и на дисплей
+
+  // выводим температуру (t) и влажность (h) на монитор порта (дебаг) и на дисплей
   /*
     Serial.print("Humidity: ");
-    Serial.println(h);
+    Serial.println(DHT11_Hum);
     Serial.print("Temperature: ");
-    Serial.println(t);
+    Serial.println(DHT11_temp);
     lcd.setCursor(0, 1);
     lcd.print("Display 0");
     Serial.println("Display 0");
   */
-  //"Рисуем" интерфейс
-  lcd.setCursor(0, 0);
-  lcd.print("Temp:");
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity:");
-  lcd.setCursor(15, 0);
-  lcd.print("C");
-  lcd.setCursor(15, 1);
-  lcd.print("%");
-  lcd.setCursor(10, 0);
-  lcd.print(DHT11_temp, 1);
-  lcd.setCursor(10, 1);
-  lcd.print(DHT11_Hum, 1);
+  if (secTimer.isReady()) {
+    //"Рисуем" интерфейс
+    lcd.setCursor(0, 0);
+    lcd.print("Temp:");
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity:");
+    lcd.setCursor(15, 0);
+    lcd.print("C");
+    lcd.setCursor(15, 1);
+    lcd.print("%");
+    lcd.setCursor(10, 0);
+    lcd.print(DHT11_temp, 1); // показать только 1 знак после зпт
+    lcd.setCursor(10, 1);
+    lcd.print(DHT11_Hum);
+  }
 
 }
 
 void Display1() {
-  unsigned long time;
-  time = micros();
-  lcd.setCursor(0, 0);
-  lcd.print(time);
-  Serial.println(time);
+  if (secTimer.isReady()) {
+    unsigned long time;
+    time = micros();
+    lcd.setCursor(0, 0);
+    lcd.print(time);
+    Serial.println(time);
+    lcd.setCursor(0, 1);
+    lcd.print("Display 1");
+  }
 
 }
 
 void Display2() {
+  lcd.setCursor(0, 1);
+  lcd.print("Display 2");
+  Serial.println("Display 2");
 }
 
 void Display3() {
   lcd.setCursor(0, 1);
-  lcd.print("Display-3");
-  Serial.println("Display-3");
+  lcd.print("Display 3");
+  Serial.println("Display 3");
 }
 
 void Button1() {
@@ -101,7 +112,9 @@ void Button1() {
 }
 
 void loop() { // Основной цикл
-  OS.tick();  // Вызов всех задач
-  power.sleepDelay(OS.getLeft());
+  //OS.tick();  // Вызов всех задач
+  sensor_DHT11();
   Button1();
+  //power.sleepDelay(OS.getLeft());
+
 }
